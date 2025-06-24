4bda897dbd9324bb2f744bcbb8306b73aa98ae73
aipPlaton <alexplaton0607@gmail.com>
2025-06-25 03:04:59 +0800

    Initial header

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import './Header.css'
+import me from './../assets/IMG_7606.JPG'
 
 export const Header = () => {
   return (
     <>
         <div className="header">
-            This is my header.
+            <div className="grid">
+                <div className="grid-left">
+                    <h1>Hello! This is Alex.</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna
+                    </p>
+                </div>
+
+                <div className="grid-right">
+                    <img src={me} style={{width: '60%'}}/>
+                </div>
+            </div>
         </div>
     </>
   )
--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import './Navbar.css'
+import nognog from './../assets/nognog-1x1.png';
 
 export const Navbar = () => {
   return (
     <>
         <div className="navbar">
-            This is my navbar
+            <div className="navbar-left">
+                <img src={nognog} style={{width: '70px'}}/>
+            </div>
+
+            <div className="navbar-right">
+                <ul>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
     </>
   )
