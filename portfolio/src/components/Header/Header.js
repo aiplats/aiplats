033ba1ef85ffa4ac373e700bc5fc567ca05e07da
aipPlaton <alexplaton0607@gmail.com>
2025-06-25 18:47:14 +0800

    Created landing page/home

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import me from './../assets/IMG_7606.JPG'
+import me from './../assets/IMG_7606-removebg-preview.png'
 
 export const Header = () => {
   return (
@@ -8,13 +8,25 @@ export const Header = () => {
         <div className="header">
             <div className="grid">
                 <div className="grid-left">
-                    <h1>Hello! This is Alex.</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna
+                    <h1>Hello! I'm Alex,</h1>
+                    <p>A passionate Full Stack Web Developer dedicated to help build functional and engaging websites, with a strong background in both Front-End & Back-End Development.  <br /> <br />
+                        I'm here to turn ideas into interactive digital experiences that are not only visually appealing but also seamlessly performant. 
                     </p>
+        
+                    <h2>Connect with me!</h2>
+
+                    <div className="grid-left-buttons">
+                        <button><span>Contact Info</span></button>
+                        <button><span>Download CV</span></button>
+                    </div>
                 </div>
 
                 <div className="grid-right">
-                    <img src={me} style={{width: '60%'}}/>
+                    <div className="image-wrapper">
+                        <img src={me} style={{width: '70%'}}/>
+                        <span className="circle"></span>
+                        <span className="circle2"></span>
+                    </div>
                 </div>
             </div>
         </div>
--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Navbar.css'
 import nognog from './../assets/nognog-1x1.png';
+import linkedin from './../assets/icons8-linkedin-48.png'
+import whatsapp from './../assets/icons8-whatsapp-48.png'
 
 export const Navbar = () => {
   return (
     <>
+    <div className='main-nav'>
         <div className="navbar">
             <div className="navbar-left">
                 <img src={nognog} style={{width: '70px'}}/>
+                <p>AIPLATS</p>
             </div>
 
             <div className="navbar-right">
@@ -17,7 +21,15 @@ export const Navbar = () => {
                     <li>Contact</li>
                 </ul>
             </div>
+
+            <div className="navbar-righter">
+                <img src={linkedin} style={{width: '40px', height: '40px'}}/>
+                <img src={whatsapp} style={{width: '40px', height: '40px'}}/>
+            </div>
         </div>
+
+        <hr />
+    </div>
     </>
   )
 }
