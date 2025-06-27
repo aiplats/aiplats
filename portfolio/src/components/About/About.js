66c18cec3ef664f282937c0bb835fcdaeddb2d51
aipPlaton <alexplaton0607@gmail.com>
2025-06-28 02:41:51 +0800

    changed background; changed formats (to be continued)

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 import gradpic from './../assets/PLATON-GRAD.jpg'
-import itflogo from './../assets/itflogo.png'
+import itflogo from './../assets/itflogo2.png'
 import adscert from './../assets/adscert.png'
 
 export const About = () => {
@@ -34,11 +34,13 @@ export const About = () => {
                     <h1>ABOUT ME</h1>
                     <div className="line"></div>
                   </div>
-                  <h2><b>Alexander Isaac P. Platon</b></h2>
-                  <p>I am an aspiring Full-Stack Web Developer, who is passionate in building and developing Web Applications. I specialize in building functional, responsive, and engaging web applications with React Framework. 
-                  <p>I have worked on projects for a client and personal projects throughout the start of my career, and my experience has helped me understand and provide solutions from different projects. </p>
-                  <p>I'm a BSIT graduate from First Asia Institute of Technology and Humanities (FAITH Colleges), where I developed a strong foundation in Web Development. From crafting clean UI to managing server-side logic. </p>
-                  </p>
+
+                  <div className="about-text">
+                    <h2><b>Alexander Isaac P. Platon</b></h2>
+                    <p>"I am an aspiring Full-Stack Web Developer, who is passionate in building and developing Web Applications. I specialize in building functional, responsive, and engaging web applications with React Framework. </p>
+                    <p>I have worked on projects for a client and personal projects throughout the start of my career, and my experience has helped me understand and provide solutions from different projects. </p>
+                    <p>I'm a BSIT graduate from First Asia Institute of Technology and Humanities (FAITH Colleges), where I developed a strong foundation in Web Development. From crafting clean UI to managing server-side logic."</p>
+                  </div>
                 </div>
             </div>
             <br/>
@@ -52,7 +54,7 @@ export const About = () => {
                       {certifications.map((cert, index) => (
                         <React.Fragment key={index}>
                           <div className="cert-icons">
-                            <img src={cert.icon} alt={`Certification ${index + 1}`} />
+                            <img src={cert.icon} className="icon-img" alt={`Certification ${index + 1}`} />
                             <h2>{cert.title}</h2>
                             <h3>{cert.credential} {cert.id}</h3>
                           </div>
--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 import me from './../assets/IMG_7606-removebg-preview.png'
+import bgrightblack from './../assets/bgrightblack.png'
 
 export const Header = () => {
   return (
@@ -8,10 +9,11 @@ export const Header = () => {
         <div className="header">
             <div className="grid">
                 <div className="grid-left">
-                    <h1>Hey there! I'm Alex,</h1>
-                    <p>A passionate Full Stack Web Developer dedicated to help build functional and engaging websites, with a strong background in both Front-End & Back-End Development.  <br /> <br />
-                        I'm here to turn ideas into interactive digital experiences that are not only visually appealing but also seamlessly performant. 
-                    </p>
+                    <div className="grid-left-tag">
+                        <h1>Hey there! I'm Alex,</h1>
+                        <p>A passionate Full Stack Web Developer dedicated to help build functional and engaging websites, with a strong background in both Front-End & Back-End Development.</p>
+
+                    </div>
         
                     <h2>Connect with me!</h2>
 
@@ -24,13 +26,10 @@ export const Header = () => {
                 <div className="grid-right">
                     <div className="image-wrapper">
                         <img src={me} style={{width: '70%'}}/>
-                        <span className="circle"></span>
-                        <span className="circle2"></span>
                     </div>
                 </div>
             </div>
         </div>
-        <br id="About"/>
     </>
   )
 }
