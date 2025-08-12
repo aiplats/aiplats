4580d14635c374e96908cd281d7a3951798b5030
aipPlaton <alexplaton0607@gmail.com>
2025-08-12 22:42:57 +0800

    fix: fixed layout @ footer

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -8,28 +8,29 @@ import github from './../assets/icons8-white-github-50.png'
 export const Footer = () => {
   return (
     <>
-        <footer>
-            <div className="main-footer">
-                <div className="footer-container">
-                    <div class="footer-name">
-                      <img src={aiplatslogo} alt='aiplats logo' /> &nbsp;
-                      © 2025 aiplats. All rights reserved.
-                    </div>
-
-                    <div class="footer-links">
-                      <a href="#About">About</a> | 
-                      <a href="#Projects">Projects</a> | 
-                      <a href="#Services">Services</a> | 
-                      <a href="#Contact">Contact</a>
-                    </div>
-
-                    <div class="footer-socials">
-                      <a href="www.linkedin.com/in/aiplats"><img src={linkedin} alt="LinkedIn" style={{width: '40px', height: '40px'}}/></a>
-                      <a href="https://github.com/aipPlaton"><img src={github} alt="GitHub" style={{width: '40px', height: '40px'}} /></a>
-                    </div>
-                </div>
+      <footer>
+        <div className="main-footer">
+          <div className="footer-container">
+            <div class="footer-name">
+              <img src={aiplatslogo} alt='aiplats logo' /> &nbsp; &nbsp;
+              © 2025 aiplats. All rights reserved.
             </div>
-        </footer>
+            
+            <img src={aiplatslogo} alt='aiplats logo' />
+            
+            <div class="footer-links">
+              <a href="#About">About</a> | 
+              <a href="#Projects">Projects</a> | 
+              <a href="#Services">Services</a> | 
+              <a href="#Contact">Contact</a>
+            </div>
+            <div class="footer-socials">
+              <a href="www.linkedin.com/in/aiplats"><img src={linkedin} alt="LinkedIn" style={{width: '40px', height: '40px'}}/></a>
+              <a href="https://github.com/aipPlaton"><img src={github} alt="GitHub" style={{width: '40px', height: '40px'}} /></a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
